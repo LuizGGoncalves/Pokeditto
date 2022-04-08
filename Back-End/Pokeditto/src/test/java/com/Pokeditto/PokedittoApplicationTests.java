59de9012468de5e5eb79bd362899e88ea243cf13
Luiz Gustavo <l.cgoncalvea@gmail.com>
2022-04-07 22:57:24 -0300

    :tada: Criaçao da divisao front-end e back-end

---
+++ Back-End/Pokeditto/src/test/java/com/Pokeditto/PokedittoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pokeditto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedittoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
