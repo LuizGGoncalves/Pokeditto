0ce8d4dad1cab07c61b7c55c7a6ea284238f5a81
marcos <marcodamasceno07@gmail.com>
2022-04-08 03:37:20 -0300

    :sparkles: create
    created button component

---
+++ frontend/src/components/button/Button.jsx
@@ -0,0 +1,7 @@
+import * as C from "./Button.styled";
+
+const Button = ({ children }) => {
+  return <C.Button>{children}</C.Button>;
+};
+
+export default Button;
---
+++ frontend/src/components/button/Button.styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+padding:8px;
+outline:none;
+border-radius:7px;
+border: 1px solid #F34538;
+font-size:.9rem;
+background-color:  #F34538;
+color: #F1F1F1;
+font-weight: 600;
+box-shadow: 0 3px 5px rgba(0,0,0,0.53);
+ cursor:pointer;
+
+:hover{
+    transition: .4s linear;
+    opacity: .7;
+}
+
+`
